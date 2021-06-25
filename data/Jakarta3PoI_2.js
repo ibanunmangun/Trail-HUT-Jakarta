cdb6cca8e51a398d20cb19213ea47a6bb1edcf40
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-06-25 21:22:46 +0700

    Add files via upload

---
+++ data/Jakarta3PoI_2.js
@@ -0,0 +1,18 @@
+var json_Jakarta3PoI_2 = {
+"type": "FeatureCollection",
+"name": "Jakarta3PoI_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Jakarta Aquarium & Safari", "desc": "Jakarta Aquarium & Safari, Neo Soho Mall - Lantai LG101 - LGM101, Jl. Letjen S. Parman No.Kav. 28, Daerah Khusus Ibukota Jakarta 11470, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.7904684, -6.1752647 ] } },
+{ "type": "Feature", "properties": { "name": "Pisang Goreng Madu Bu Nanik.", "desc": "Pisang Goreng Madu Bu Nanik, Jl. Tanjung Duren Raya No.67, South Tanjung Duren, Grogol petamburan, West Jakarta City, Jakarta 11470, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.7836935, -6.1747314 ] } },
+{ "type": "Feature", "properties": { "name": "Museum Macan (Modern and Contemporary Art in Nusantara)", "desc": "Museum MACAN (Modern and Contemporary Art in Nusantara), AKR Tower, Jl. Perjuangan No.5, RT.11\/RW.10, Kb. Jeruk, Kec. Kb. Jeruk, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11530, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.7676201, -6.1909003 ] } },
+{ "type": "Feature", "properties": { "name": "Kuliner Bendungan Hilir", "desc": "Benhill, Jl. Bendungan Hilir No.44, RT.5\/RW.2, Bend. Hilir, Kecamatan Tanah Abang, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10210, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.814545481838891, -6.214454636943788 ] } },
+{ "type": "Feature", "properties": { "name": "MOJA Museum", "desc": "MOJA Museum, Gelora Bung Karno Sports Complex, Main Stadium, Jl. Gerbang Pemuda No.1, RW.3, Gelora, Daerah Khusus Ibukota Jakarta 10270, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8034472, -6.2174531 ] } },
+{ "type": "Feature", "properties": { "name": "M Bloc Space", "desc": "M Bloc Space, Panglima Polim St No.37, RW.1, Melawai, Kebayoran Baru, South Jakarta City, Jakarta 12160, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.7986948, -6.2413576 ] } },
+{ "type": "Feature", "properties": { "name": "Mushola Neo Soho", "desc": null, "X": null, "Y": null, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.7899144, -6.174225532 ] } },
+{ "type": "Feature", "properties": { "name": "SPBU Pertamina 34-10201", "desc": null, "X": null, "Y": null, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.8061459, -6.202154828 ] } },
+{ "type": "Feature", "properties": { "name": "Pos Polisi M Bloc Space", "desc": null, "X": null, "Y": null, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.7988509, -6.239464736 ] } },
+{ "type": "Feature", "properties": { "name": "Rumah Sakit Pusat Pertamina", "desc": null, "X": null, "Y": null, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.7950744, -6.238952341 ] } },
+{ "type": "Feature", "properties": { "name": "Rumah Sakit TNI Angkatan Laut Dr. Mintohardjo", "desc": null, "X": null, "Y": null, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.8117544, -6.210706176 ] } }
+]
+}
